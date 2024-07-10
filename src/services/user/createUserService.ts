fd56ef7627534417de0a841d6944777e44ae0668
Tiago <tiagobnarita@gmail.com>
2024-07-09 23:12:31 -0300

    user created

--- src/services/user/createUserService.ts
+++ src/services/user/createUserService.ts
@@ -1,14 +1,42 @@
+import { prismaClient } from "../../prisma";
+
 interface UserRequest {
-  name: String;
-  email: String;
-  password: String;
+  name: string;
+  email: string;
+  password: string;
 }
 
 class CreateUserService {
   async execute({ name, email, password }: UserRequest) {
-    console.log(name);
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
 
-    return { ok: true };
+    //verifica se ja existte algum usuario
+    const userAlreadyExist = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userAlreadyExist) {
+      throw new Error("User already exist");
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
   }
 }
 
