567ed8889a9c67a1d35946906ed6137a176785a2
Tiago <tiagobnarita@gmail.com>
2024-07-09 23:45:28 -0300

    creating auth user

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/createUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 const router = Router();
 
 // user routes
 router.post("/users", new CreateUserController().handle);
+
+router.post("/session", new AuthUserController().handle);
 
 export { router };
---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,14 @@
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthRequest) {
+    console.log(email);
+
+    return { ok: true };
+  }
+}
+
+export { AuthUserService };
