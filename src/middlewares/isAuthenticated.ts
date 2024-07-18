89a21b7a6edb9531986302c00fdf0d56ee306291
Tiago <tiagobnarita@gmail.com>
2024-07-17 22:43:17 -0300

    add middleware

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -16,7 +16,11 @@ export function isAuthenticated(req: Request, resp: Response, next: NextFunction
   const [, token] = authToken.split(" ");
 
   try {
-    const {} = verify();
+    const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
+
+    req.user_id = sub;
+
+    return next();
   } catch (err) {
     return resp.status(401).end();
   }
